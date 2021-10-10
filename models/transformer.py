6bfa75248e8ed119f12771ffa5fddca3589e2281
Sumanas Sarma <252960+insectatorious@users.noreply.github.com>
2021-10-10 08:23:10 +0100

    Fix U-shape of network output shapes
    These changes bring the model structure closer to what was proposed.

--- models/transformer.py
+++ models/transformer.py
@@ -14,44 +14,66 @@ from tensorflow_addons.layers import InstanceNormalization
 
 class ResidualBlock(tf.keras.layers.Layer):
 
-  def __init__(self, num_of_filters: int = 16, **kwargs):
+  def __init__(self,
+               num_of_filters: int = 16,
+               norm_type: str = "batch",
+               final_relu: bool = True,
+               **kwargs):
+    if norm_type not in ["batch", "instance"]:
+      raise AttributeError(f"Expected 'norm_type' to be one of 'batch' or "
+                           f"'instance'. Got '{norm_type}")
+
     super(ResidualBlock, self).__init__(**kwargs)
     self.num_of_filters = num_of_filters
+    self.norm_type = norm_type
+    self.final_relu = final_relu
     self.conv_1 = None
     self.conv_2 = None
     self.relu_1 = None
     self.relu_2 = None
-    self.batch_norm_1 = None
-    self.batch_norm_2 = None
-    self.add_1 = None
+    self.norm_1 = None
+    self.norm_2 = None
+    if self.final_relu:
+      self.add_1 = None
 
   def build(self, input_shape: List) -> None:
     self.conv_1 = Conv2D(filters=self.num_of_filters,
                          kernel_size=(1, 1),
                          input_shape=input_shape)
-    self.batch_norm_1 = BatchNormalization()
+
+    if self.norm_type == "batch":
+      self.norm_1 = BatchNormalization()
+    else:
+      self.norm_1 = InstanceNormalization()
     self.relu_1 = LeakyReLU()
     self.conv_2 = Conv2D(filters=self.num_of_filters,
                          kernel_size=3,
                          padding="same")
-    self.batch_norm_2 = BatchNormalization()
+    if self.norm_type == "batch":
+      self.norm_2 = BatchNormalization()
+    else:
+      self.norm_2 = InstanceNormalization()
     self.add_1 = Add()
-    self.relu_2 = LeakyReLU()
+    if self.final_relu:
+      self.relu_2 = LeakyReLU()
 
   def call(self, inputs: Tensor) -> Tensor:
     layer = self.conv_1(inputs)
-    layer = self.batch_norm_1(layer)
+    layer = self.norm_1(layer)
     layer = self.relu_1(layer)
     layer = self.conv_2(layer)
-    layer = self.batch_norm_2(layer)
+    layer = self.norm_2(layer)
     layer = self.add_1([layer, inputs])
-    layer = self.relu_2(layer)
+    if self.final_relu:
+      layer = self.relu_2(layer)
 
     return layer
 
   def get_config(self) -> Dict:
     config = super(ResidualBlock, self).get_config()
-    config.update({"num_of_filters": self.num_of_filters})
+    config.update({"num_of_filters": self.num_of_filters,
+                   "norm_type": self.norm_type,
+                   "final_relu": self.final_relu})
 
     return config
 
@@ -95,43 +117,54 @@ class Transformer(tf.keras.Model):
     self.stride_sizes = [1, 2, 2]
     self.conv1 = Conv2D(self.num_of_channels[1],
                         kernel_size=self.kernel_sizes[0],
+                        padding="same",
                         strides=self.stride_sizes[0])
     self.instance_norm_1 = InstanceNormalization()
     self.relu_1 = LeakyReLU()
     self.conv2 = Conv2D(self.num_of_channels[2],
                         kernel_size=self.kernel_sizes[1],
+                        padding="same",
                         strides=self.stride_sizes[1])
     self.instance_norm_2 = InstanceNormalization()
     self.relu_2 = LeakyReLU()
     self.conv3 = Conv2D(self.num_of_channels[3],
-                        kernel_size=self.kernel_sizes[2])
+                        padding="same",
+                        kernel_size=self.kernel_sizes[2],
+                        strides=self.stride_sizes[2])
     self.instance_norm_3 = InstanceNormalization()
     self.relu_3 = LeakyReLU()
 
     residual_block_filters = 128
-    self.res_block_1 = ResidualBlock(residual_block_filters)
-    self.res_block_2 = ResidualBlock(residual_block_filters)
-    self.res_block_3 = ResidualBlock(residual_block_filters)
-    self.res_block_4 = ResidualBlock(residual_block_filters)
-    self.res_block_5 = ResidualBlock(residual_block_filters)
+    self.res_block_1 = ResidualBlock(residual_block_filters, "instance", False)
+    self.res_block_2 = ResidualBlock(residual_block_filters, "instance", False)
+    self.res_block_3 = ResidualBlock(residual_block_filters, "instance", False)
+    self.res_block_4 = ResidualBlock(residual_block_filters, "instance", False)
+    self.res_block_5 = ResidualBlock(residual_block_filters, "instance", False)
+
+    # For upsampling, we need to use a combination of the UpSampling2D followed
+    # by a Conv2D layer to match his UpsampleConvLayer
+    # See https://github.com/gordicaleksa/pytorch-neural-style-transfer-johnson/blob/00c96e8e3f1b0b7fb4c14254fd0c6f1281a29598/models/definitions/transformer_net.py#L106
 
     self.upsample_1 = UpSampling2D(size=self.stride_sizes[-1],
                                    interpolation="nearest")
     self.instance_norm_4 = InstanceNormalization()
     self.relu_4 = LeakyReLU()
-    self.conv4 = Conv2D(self.num_of_channels[-1],
+    self.conv4 = Conv2D(self.num_of_channels[-2],
                         kernel_size=self.kernel_sizes[-1],
-                        strides=self.stride_sizes[-1])
+                        padding="same")
 
     self.upsample_2 = UpSampling2D(size=self.stride_sizes[-2],
                                    interpolation="nearest")
     self.instance_norm_5 = InstanceNormalization()
     self.relu_5 = LeakyReLU()
-    self.upsample_3 = UpSampling2D(size=self.stride_sizes[-3],
-                                   interpolation="nearest")
-    self.conv5 = Conv2D(self.num_of_channels[0],
-                        kernel_size=self.kernel_sizes[0],
-                        strides=self.stride_sizes[0])
+    self.conv5 = Conv2D(self.num_of_channels[-3],
+                        kernel_size=self.kernel_sizes[-2],
+                        padding="same")
+
+    self.conv6 = Conv2D(self.num_of_channels[-4],
+                        kernel_size=self.kernel_sizes[-3],
+                        strides=self.stride_sizes[-3],
+                        padding="same")
 
   def call(self, inputs: Tensor) -> Tensor:
     x = self.relu_1(self.instance_norm_1(self.conv1(inputs)))
@@ -145,6 +178,6 @@ class Transformer(tf.keras.Model):
     x = self.res_block_5(x)
 
     x = self.conv4(self.relu_4(self.instance_norm_4(self.upsample_1(x))))
-    x = self.relu_5(self.instance_norm_5(self.upsample_2(x)))
+    x = self.conv5(self.relu_5(self.instance_norm_5(self.upsample_2(x))))
 
-    return self.conv5(self.upsample_3(x))
+    return self.conv6(x)
