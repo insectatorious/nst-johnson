cd9b2ef7a14331a40ab3ca407a7238ae3d04d02f
Sumanas Sarma <252960+insectatorious@users.noreply.github.com>
2021-10-10 08:21:51 +0100

    Show layer output shapes via model.summary()
    See https://stackoverflow.com/a/65956968 for more details.

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ inputs = tf.random.uniform([1, 224, 224, 3])
 model: tf.keras.Model = Transformer()
 
 model.build(input_shape=(None, 224, 224, 3))
+model.call(tf.keras.layers.Input(shape=(224, 224, 3)))
 model.summary()
 
 y = model.predict(inputs)
