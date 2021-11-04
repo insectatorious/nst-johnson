836815551b4e284d39f58ebd3e5761ab13553030
Sumanas Sarma <252960+insectatorious@users.noreply.github.com>
2021-11-04 23:49:08 +0000

    Refactor style and content loss functions

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,15 @@
 import os
-from typing import Tuple
+from typing import Tuple, List
 
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras, Tensor
-from tensorflow.keras.applications import vgg19
+from tensorflow.keras.applications import vgg19, vgg16
+from tensorflow.python.keras.losses import MeanSquaredError
+from tensorflow.keras.losses import Reduction
 from tqdm import tqdm
+
+from models.perceptual_loss import PerceptualModelType
 
 
 def get_img_dimensions(img_path: str) -> Tuple[int, int]:
@@ -14,23 +18,44 @@ def get_img_dimensions(img_path: str) -> Tuple[int, int]:
   return width, height
 
 
-def preprocess_image(image_path, img_nrows, img_ncols):
-  # Util function to open, resize and format pictures into appropriate tensors
+def preprocess_image(image_path: str,
+                     img_nrows: int,
+                     img_ncols: int,
+                     model_type: PerceptualModelType = PerceptualModelType.VGG_19):
+  """Util function to open, resize and format pictures into appropriate tensors"""
+
+  if model_type == PerceptualModelType.VGG_19:
+    preprocess_fn = vgg19.preprocess_input
+  elif model_type == PerceptualModelType.VGG_16:
+    preprocess_fn = vgg16.preprocess_input
+  else:
+    preprocess_fn = lambda x: x
+
   img = keras.preprocessing.image.load_img(image_path,
                                            target_size=(img_nrows, img_ncols))
   img = keras.preprocessing.image.img_to_array(img)
   img = np.expand_dims(img, axis=0)
-  img = vgg19.preprocess_input(img)
+  img = preprocess_fn(img)
 
   return tf.convert_to_tensor(img)
 
 
-def preprocess_img_tensor(img_batch, img_nrows, img_ncols) -> Tensor:
+def preprocess_img_tensor(img_batch: Tensor,
+                          img_nrows: int,
+                          img_ncols: int,
+                          model_type: PerceptualModelType = PerceptualModelType.VGG_19) -> Tensor:
   # tf.expand_dims(img_batch, a)
   # No need to expand dims as img_batch is a list of images
   img_batch = tf.image.resize(img_batch, (img_nrows, img_ncols))
 
-  return vgg19.preprocess_input(img_batch)
+  if model_type == PerceptualModelType.VGG_19:
+    preprocess_fn = vgg19.preprocess_input
+  elif model_type == PerceptualModelType.VGG_16:
+    preprocess_fn = vgg16.preprocess_input
+  else:
+    preprocess_fn = lambda x: x
+
+  return preprocess_fn(img_batch)
 
 
 def deprocess_image(x, img_nrows, img_ncols, add_offset: bool = True):
@@ -64,29 +89,6 @@ def get_pbar(total: int = 100):
   return tqdm(total=total, leave=False, ascii=True)
 
 
-"""## Compute the style transfer loss
-
-First, we need to define 4 utility functions:
-
-- `gram_matrix` (used to compute the style loss)
-- The `style_loss` function, which keeps the generated image close to the local textures of the style reference image
-- The `content_loss` function, which keeps the high-level representation of the
-generated image close to that of the base image
-- The `total_variation_loss` function, a regularization loss which keeps the generated image locally-coherent
-"""
-
-
-# The gram matrix of an image tensor (feature-wise outer product)
-
-
-def gram_matrix(x):
-  x = tf.transpose(x, (2, 0, 1))
-  features = tf.reshape(x, (tf.shape(x)[0], -1))
-  gram = tf.matmul(features, tf.transpose(features))
-
-  return gram
-
-
 def batch_gram_matrix(x, normalise: bool = True):
   x = tf.transpose(x, (0, 3, 1, 2))
   (b, h, w, ch) = x.shape.as_list()
@@ -98,44 +100,6 @@ def batch_gram_matrix(x, normalise: bool = True):
   gram = gram / (ch * h * w) if normalise else gram
 
   return gram
-
-
-# The "style loss" is designed to maintain
-# the style of the reference image in the generated image.
-# It is based on the gram matrices (which capture style) of
-# feature maps from the style reference image
-# and from the generated image
-
-
-def style_loss(style, combination, img_ncols, img_nrows):
-  S = gram_matrix(style)
-  C = gram_matrix(combination)
-  channels = 3
-  size = img_nrows * img_ncols
-
-  return tf.reduce_sum(tf.square(S - C)) / (4.0 * (channels ** 2) * (size ** 2))
-
-
-# An auxiliary loss function
-# designed to maintain the "content" of the
-# base image in the generated image
-
-def content_loss(base, combination):
-  return tf.reduce_sum(tf.square(combination - base))
-
-
-# The 3rd loss function, total variation loss,
-# designed to keep the generated image locally coherent
-
-
-def total_variation_loss(x, img_ncols, img_nrows, l2: bool = True):
-  l_loss = tf.math.square if l2 else tf.math.abs
-  a = l_loss(
-    x[:, :img_nrows - 1, :img_ncols - 1, :] - x[:, 1:, :img_ncols - 1, :])
-  b = l_loss(
-    x[:, :img_nrows - 1, :img_ncols - 1, :] - x[:, :img_nrows - 1, 1:, :])
-
-  return tf.reduce_sum(tf.pow(a + b, 1.25))
 
 
 def get_training_strategy(config):
@@ -154,3 +118,56 @@ def get_training_strategy(config):
     print("Not running on TPU...")
     config["tpu"] = False
   return strategy
+
+
+def content_loss(content_batch_feature_maps: List[Tensor],
+                 stylised_batch_feature_maps: List[Tensor]) -> Tensor:
+  loss = tf.zeros(shape=())
+  content_weights = (1., 1., 1.)
+  i = 0
+  for (target_content_representation,
+       current_content_representation) in zip(content_batch_feature_maps,
+                                              stylised_batch_feature_maps):
+    loss += content_weights[i] * tf.reduce_sum(
+      tf.square(target_content_representation - current_content_representation))
+    i += 1
+
+  return loss
+
+
+def style_loss(target_style_representation: List[Tensor],
+               stylised_batch_feature_maps: List[Tensor],
+               channels: int = 3) -> Tensor:
+  loss = tf.zeros(shape=())
+  current_style_representation = [batch_gram_matrix(x, normalise=True)
+                                  for x in stylised_batch_feature_maps]
+  for gram_gt, gram_hat in zip(target_style_representation,
+                               current_style_representation):
+    loss += MeanSquaredError(reduction=Reduction.SUM)(gram_gt, gram_hat)
+    # S, C = gram_gt, gram_hat
+    # # print("@@@@@", gram_gt.shape)
+    # size = gram_gt.shape[1] * gram_gt.shape[2]
+    # loss += tf.reduce_sum(tf.square(S - C)) / (4.0 * (channels ** 2) * (size ** 2))
+
+  loss /= len(target_style_representation)
+
+  return loss
+
+
+def total_variation_loss(stylised_batch):
+  # Small change 👇 to reduce_sum instead of mean
+  return tf.reduce_sum(tf.image.total_variation(stylised_batch))
+
+
+def compute_losses(stylised_batch,
+                   target_style_representation,
+                   content_feature_maps,
+                   stylised_content_feature_maps,
+                   stylised_style_feature_maps) -> Tuple[Tensor, Tensor, Tensor]:
+  batch_content_loss = content_loss(content_batch_feature_maps=content_feature_maps,
+                                    stylised_batch_feature_maps=stylised_content_feature_maps)
+  batch_style_loss = style_loss(target_style_representation=target_style_representation,
+                                stylised_batch_feature_maps=stylised_style_feature_maps)
+  batch_total_variation_loss = total_variation_loss(stylised_batch=stylised_batch)
+
+  return batch_content_loss, batch_style_loss, batch_total_variation_loss
